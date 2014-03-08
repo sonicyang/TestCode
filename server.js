9e077802d3be570f2973d5f61853c5cc686ffe0f
anarchih <anarchih.tw@gmail.com>
2014-03-08 14:15:59 +0800

    used express to write server.js

--- server.js
+++ server.js
@@ -1,47 +1,14 @@
-var fs = require('fs');
-var server,
-    ip   = "127.0.0.1",
-    port = 1337,
-    http = require('http'),
-    url = require('url');
-
-function onRequest(request, response) {
-  var pathname = url.parse(request.url).pathname; 
-  console.log("Request for " + pathname + " received.");
-  response.writeHead(200, {"Content-Type": "text/html"});
-  
-  switch (pathname) {
-    case "/index":
-        fs.readFile('./index.html', function(error, content) {
-		if(error){
-			response.writeHead(500);
-			response.end();
-			
-		}
-		else {
-			response.writeHead(200, { 'Content-Type': 'text/html' });	
-			response.end(content, 'utf-8');	
-				
-		}
-	});
-	break;
-    case "/test":
-  	fs.readFile('./test.html', function(error, content) {
-		if (error) {
-			response.writeHead(500);
-			response.end();
-		}
-		else {
-			response.writeHead(200, { 'Content-Type': 'text/html' });
-			console.log(content.ctx)
-			response.end(content, 'utf-8');
-		}
-	});
-      break;
-    default:
-      response.end('default page.\n');
-      break;
-  }
-}
-http.createServer(onRequest).listen(port, ip);
-console.log("Server has started.");
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+app.get('/',function(request, response){
+	response.end('你好！');
+});
+app.get('/index',function(request, response){
+  	response.end("ss");
+//	console.log(request.body.test.ctx);
+});
+server.listen(8080,'127.0.0.1',function(){
+	console.log('Server Running');
+});
