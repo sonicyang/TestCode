2711bff110ca5a3b3898213040c60a61b87dc4ce
anarchih <anarchih.tw@gmail.com>
2014-03-08 23:32:44 +0800

    added regex.js (just a test)

---
+++ regex.js
@@ -0,0 +1,10 @@
+fs = require("fs");
+var sp;
+var re = /import[\s\t]/g;
+var re2= /\os|sys[.\s]/;
+code = "import os \nfor i in range(0,10):\n\tfor j in range(0,10):\n\t\tprint (i,j)\nprint (\"end\")"
+//code = code.replace(/\t/g,"")
+sp = code.split("\n");
+console.log(re.test(sp[0]))
+console.log(re2.test(sp[0]))
+console.log(sp);
