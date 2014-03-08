209422d3e2e2e7b514bfdf3d0e69a396b3e7af1b
Sonic Yang <sonic.tw.tp@gmail.com>
2014-03-08 16:50:53 +0800

    read posted data

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+var fs = require('fs');
 var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.createServer(app);
-app.get('/',function(request, response){
-	response.end('你好！');
+
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.logger());
 });
-app.get('/index',function(request, response){
-  	response.end("ss");
+
+app.get('/',function(request, response){
+    //response.end('你好！');
+    fs.createReadStream('./index.html').pipe(response); 
+});
+app.post('/index',function(request, response){
+    console.log(request.param("ctx"));
+    //fs.writeFile('message.txt', request., function (err) {
+    //    if (err) throw err;
+    //    response.end("Saved");
+    //    });
 //	console.log(request.body.test.ctx);
 });
 server.listen(8080,'127.0.0.1',function(){
-	console.log('Server Running');
+    console.log('Server Running');
 });
