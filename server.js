91520ee266815dacf18024a81f8c2572c42df70f
Sonic Yang <sonic.tw.tp@gmail.com>
2014-03-10 21:40:29 +0800

    sync Execution implemented; funtionlized excution
    using Shelljs

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var fs = require('fs');
+var crypto = require('crypto');
+var sys = require('sys');
+var shell = require('shelljs/global');
 var http = require('http');
 var express = require('express');
+
 var app = express();
 var server = http.createServer(app);
+
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.logger());
+});
+
 function checkCode(data){
     var sp   = data.split("\n");
     var reg1 = /import[\s\t]/;
@@ -16,12 +27,9 @@ function checkCode(data){
     }	    
     return true;		
 }
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(app.router);
-    app.use(express.logger());
-});
+
 app.get('/',function(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
     fs.createReadStream('./index.html').pipe(response); 
 });
 
@@ -29,29 +37,28 @@ app.get('/jquery',function(request, response){
     fs.createReadStream('./static/jQuery/jquery-1.11.0.min.js').pipe(response);
 });
 
-var filenum = 0;
-app.post('/index',function(request, response){
-    fs.writeFile('uploaded/' + (filenum).toString() + '.py', request.param("ctx"), function (err) {
-        if (err) throw err;
-	var sys = require('sys');
-        var result = "";
-        var exec = require('child_process').exec;
-        exec('python3 uploaded/' + filenum.toString() + '.py', function callback(error, stdout, stderr){
-            sys.puts("Python STDOUT:");
-            sys.puts(stdout);    
-            sys.puts("Python STDERR:");
-            sys.puts(stderr);    
-            result = stdout.toString() + stderr.toString();
-            sys.puts(result);
-            response.end(result);
 
-            fs.unlink('uploaded/' + (filenum++).toString() + '.py', function (err) {
-                if (err) throw err;
-                console.log('successfully deleted ' + 'uploaded/' + (filenum -1).toString() + '.py');
-                });
-            });
-        });
-//	console.log(request.body.test.ctx);
+function execCode(CC, code){
+    var cc_hash = crypto.createHash('md5').update(CC).digest('hex');
+    var code_hash = crypto.createHash('md5').update(code).digest('hex');
+    
+    fs.writeFileSync('uploaded/' + cc_hash.toString() + "/" + code_hash.toString(), code);
+
+    var result = exec(CC + ' uploaded/' + cc_hash.toString() + "/" + code_hash.toString(),{silent:true}).output;
+
+    sys.puts("Executed " +  cc_hash.toString() + "/" + code_hash.toString());
+    sys.puts("Output:");
+    sys.puts(result);    
+
+    fs.unlinkSync('uploaded/' + cc_hash.toString() + "/" + code_hash.toString());
+    console.log('successfully deleted ' + 'uploaded/' + cc_hash.toString() + "/" + code_hash.toString());
+
+
+}
+
+app.post('/index',function(request, response){
+    execCode('Python', request.param("ctx"));
+    //	console.log(request.body.test.ctx);
 });
 server.listen(8080,'127.0.0.1',function(){
     console.log('Server Running');
